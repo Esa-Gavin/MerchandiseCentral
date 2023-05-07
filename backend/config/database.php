4240d5b3652daf661d4085ccc80b42431bbe7b5f
Gavin Kariuki <gavin.kariuki@student.moringaschool.com>
2023-05-07 23:48:34 +0300

    configure the connection to the mysql database using php data objects

---
+++ backend/config/database.php
@@ -0,0 +1,29 @@
+<?php
+// 🌚 this file is for configuring a connection to the database //
+// by using a PDO(PHP Data Objects) //
+class Database
+{
+    private $host = 'myapp.local';
+    private $db_name = 'scandiweb';
+    private $username = 'esa';
+    private $password = 'mzabibu21';
+    public $conn;
+
+    public function getConnection()
+    {
+        $this->conn = null;
+
+        try {
+            $this->conn = new PDO(
+                "mysql:host=" . $this->host . ";dbname" . $this->db_name,
+                $this->username,
+                $this->password
+            );
+            $this->conn->exec("set names utf8");
+        }catch (PDOException $exception) {
+            echo "Connection erro: " . $exception->getMessage();
+        }
+
+        return $this->conn;
+    }
+}
