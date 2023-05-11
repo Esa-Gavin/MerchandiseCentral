ab1ec57092387a359fb07a9015319c8dd5c6f011
Gavin Kariuki <gavin.kariuki@student.moringaschool.com>
2023-05-11 22:09:18 +0300

    add abstract crud methods

--- backend/classes/abstract/AbstractProduct.php
+++ backend/classes/abstract/AbstractProduct.php
@@ -13,4 +13,28 @@ abstract class AbstractProduct {
     public function getSku() {
         return $this->sku;
     }
+
+    public function setSku($sku) {
+        $this->sku = $sku;
+    }
+
+    public function getName() {
+        return $this->name;
+    }
+
+    public function setName($name) {
+        $this->name = $name;
+    }
+
+    public function getPrice() {
+        return $this->price;
+    }
+
+    public function setPrice($price) {
+        $this->price = $price;
+    }
+
+    // 👇 Abstract CRUD methods
+    abstract public function create();
+    abstract public function read();
 }
